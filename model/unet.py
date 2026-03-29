ef40706e0b5f1c22de5d7a20201897d3e47455ae
Isabelle Bicudo <isabellebicudo70@gmail.com>
2026-03-29 15:54:22 -0400

    fix(modelo): correção da arquitetura da U-Net e conexões skip
    * Corrige dimensões dos canais na rede
    * Ajusta skip connections entre encoder e decoder
    * Substitui Upsample por ConvTranspose2d
    * Adiciona BatchNorm para maior estabilidade

--- model/unet.py
+++ model/unet.py
@@ -3,40 +3,37 @@ import torch.nn as nn
 
 class UNet(nn.Module):
     def __init__(self, n_channels=3, n_classes=2):
-        super(UNet, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
+        super().__init__()
 
-        def double_conv(in_channels, out_channels):
+        def double_conv(in_c, out_c):
             return nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+                nn.Conv2d(in_c, out_c, 3, padding=1),
+                nn.BatchNorm2d(out_c),
                 nn.ReLU(inplace=True),
-                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True)
-            )
-
-        def down(in_channels, out_channels):
-            return nn.Sequential(
-                nn.MaxPool2d(2),
-                double_conv(in_channels, out_channels)
-            )
-
-        def up(in_channels, out_channels):
-            return nn.Sequential(
-                nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
-                double_conv(in_channels, out_channels)
+                nn.Conv2d(out_c, out_c, 3, padding=1),
+                nn.BatchNorm2d(out_c),
+                nn.ReLU(inplace=True),
             )
 
         self.inc = double_conv(n_channels, 64)
-        self.down1 = down(64, 128)
-        self.down2 = down(128, 256)
-        self.down3 = down(256, 512)
-        self.down4 = down(512, 1024)
-        self.up1 = up(1024, 512)
-        self.up2 = up(1024, 256)
-        self.up3 = up(512, 128)
-        self.up4 = up(256, 64)
-        self.outc = nn.Conv2d(128, n_classes, kernel_size=1)
+        self.down1 = nn.Sequential(nn.MaxPool2d(2), double_conv(64, 128))
+        self.down2 = nn.Sequential(nn.MaxPool2d(2), double_conv(128, 256))
+        self.down3 = nn.Sequential(nn.MaxPool2d(2), double_conv(256, 512))
+        self.down4 = nn.Sequential(nn.MaxPool2d(2), double_conv(512, 1024))
+
+        self.up1 = nn.ConvTranspose2d(1024, 512, 2, stride=2)
+        self.conv1 = double_conv(1024, 512)
+
+        self.up2 = nn.ConvTranspose2d(512, 256, 2, stride=2)
+        self.conv2 = double_conv(512, 256)
+
+        self.up3 = nn.ConvTranspose2d(256, 128, 2, stride=2)
+        self.conv3 = double_conv(256, 128)
+
+        self.up4 = nn.ConvTranspose2d(128, 64, 2, stride=2)
+        self.conv4 = double_conv(128, 64)
+
+        self.outc = nn.Conv2d(64, n_classes, 1)
 
     def forward(self, x):
         x1 = self.inc(x)
@@ -44,13 +41,21 @@ class UNet(nn.Module):
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
+
         x = self.up1(x5)
         x = torch.cat([x, x4], dim=1)
+        x = self.conv1(x)
+
         x = self.up2(x)
         x = torch.cat([x, x3], dim=1)
+        x = self.conv2(x)
+
         x = self.up3(x)
         x = torch.cat([x, x2], dim=1)
+        x = self.conv3(x)
+
         x = self.up4(x)
         x = torch.cat([x, x1], dim=1)
-        logits = self.outc(x)
-        return logits
+        x = self.conv4(x)
+
+        return self.outc(x)
